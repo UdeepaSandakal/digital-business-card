c471cfdbac8d8e10c1a9f1db1cef0804fef7d7a5
Udeepa Sandakal <udeepasd@gmail.com>
2024-05-10 16:39:31 +0530

    info-section-done

---
+++ src/components/Info.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Profile from "../assets/Profile_Pic.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+function Info() {
+    return(
+        <div className="info"> 
+            <img src={Profile} alt="Udeepa Sandakal" />
+            <h1 className="info--fullname">Udeepa Sandakal</h1>
+            <h5 className="info--role">Self Learn Full-Stack Developer</h5>
+            <a className="info--link" href="" target="blank">KeeDev.io</a>
+        
+            <div className="info--buttons">
+                <address>
+                    <a href="udeepasd@gmail.com">
+                        <button className="btn btn--email">
+                            <FontAwesomeIcon icon={faEnvelope} className="info--icon"/>Email
+                        </button> 
+                    </a>    
+                </address>
+
+                <a href="https://lk.linkedin.com/in/udeepa-sandakal" target="blank">
+                    <button className="btn btn--linkedin">
+                        <FontAwesomeIcon icon={faLinkedin} className="info--icon"/>Linkedin
+                    </button>
+                </a>
+
+            </div>        
+        </div>
+    )
+}
+
+export default Info
---
+++ src/components/Interest.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Interest() {
+
+}
+
+export default Interest
